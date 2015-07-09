a079c66838a7eeb006584210e3246d47003455c3
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-07-09 18:00:11 -0400

    updated answer associations"

--- stacksonstacks/app/models/answer.rb
+++ stacksonstacks/app/models/answer.rb
@@ -2,7 +2,6 @@ class Answer < ActiveRecord::Base
   has_many :votes, as: :votable
   has_many :comments, as: :commentable
   belongs_to :question
-  has_one :best_answered_question, class_name: "Question", foreign_key: "answer_id"
   belongs_to :user
   validates :body, presence: true
 end
