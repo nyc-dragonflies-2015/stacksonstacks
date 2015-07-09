f129a4d1115ae75b6d19f25ebb9593451279ee56
grapefruitricky <arthur.ross.wilson@gmail.com>
2015-07-09 18:01:19 -0400

    updated user associations

--- stacksonstacks/app/models/user.rb
+++ stacksonstacks/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
   has_many :favorite_questions
+  has_many :favorited_questions, through: :favorite_questions, source: "question"
   has_many :comments
   has_many :votes
   validates :username, uniqueness: true, presence: true
