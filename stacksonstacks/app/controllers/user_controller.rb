7d0b0365bbb02331692c8f93212e66f3885a85d3
Marc Cardacci <marc.cardacci@gmail.com>
2015-07-10 17:54:59 -0400

    makes signup method

--- stacksonstacks/app/controllers/user_controller.rb
+++ stacksonstacks/app/controllers/user_controller.rb
@@ -4,8 +4,23 @@ class UserController < ApplicationController
     @user = User.find_by(id: params[:id] )
   end
 
+  def signup
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:notice] = "There was a problem with your account info."
+      redirect_to :back
+    end
+  end
+
   private
     def user_params
-      params.require(:user).permit(:username, :password, :reputation)
+      params.require(:user).permit(:username, :password)
     end
 end
