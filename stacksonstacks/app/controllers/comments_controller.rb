c15bab7cd0b7caac7c173bd2053cf34e4cf62ad3
julia-castro <julia.castro@outlook.com>
2015-07-11 17:42:30 -0400

    functionality added: add comment to answer on question show page

--- stacksonstacks/app/controllers/comments_controller.rb
+++ stacksonstacks/app/controllers/comments_controller.rb
@@ -4,14 +4,30 @@ class CommentsController < ApplicationController
     @comments = Comment.all
   end
 
+  def new
+    #this is only for adding a comment to an ANSWER!
+    @comment = Comment.new
+    @id = session[:user_id]
+    @answer_id = params["answer_id"]
+  end
+
   def create
     @comment = Comment.new(comment_params)
-    @question = Question.find_by(id: comment_params[:commentable_id])
-    if @question.comments << @comment
-      redirect_to :back
-    else
-      flash[:notice] = "There was a problem submitting your comment."
-      redirect_to :back
+    if comment_params[:commentable_type] == "question"
+      @question = Question.find_by(id: comment_params[:commentable_id])
+      if @question.comments << @comment
+        redirect_to :back
+      else
+        flash[:notice] = "There was a problem submitting your comment."
+        redirect_to :back
+      end
+    elsif comment_params[:commentable_type] == "answer"
+      @answer = Answer.find_by(id: comment_params[:commentable_id])
+      if @answer.comments << @comment
+        redirect_to question_path(@answer.question.id)
+      else
+        redirect_to :back
+      end
     end
   end
 
