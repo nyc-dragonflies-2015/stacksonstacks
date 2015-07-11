288ad1d1bd3db0ea342147f9c445346cdbb1cd81
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-07-11 16:35:55 -0400

    remove the model methods

--- stacksonstacks/app/models/vote.rb
+++ stacksonstacks/app/models/vote.rb
@@ -2,11 +2,4 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true
   belongs_to :user
 
-  def current_user
-    return User.find_by(id: session[:user_id])
-  end
-
-  def user_has_made_this_vote(question_or_answer, upvote?)
-    user_array = question_or_answer.votes.map {|vote| vote.user}
-    return true if user_array.
 end
