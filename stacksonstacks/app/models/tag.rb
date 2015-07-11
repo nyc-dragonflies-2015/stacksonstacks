59ebe5c739965eeb0cb5a553f102ec0aa0baf24b
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-07-10 20:16:19 -0400

    update order tags

--- stacksonstacks/app/models/tag.rb
+++ stacksonstacks/app/models/tag.rb
@@ -2,8 +2,8 @@ class Tag < ActiveRecord::Base
   has_many :question_tags
   has_many :questions, through: :question_tags
 
-  def order_tags
-    return Tags.all.sort_by {|tag| tag.questions.length}
+  def self.order_tags
+    return Tag.all.sort_by {|tag| tag.questions.length}
   end
 
 end
