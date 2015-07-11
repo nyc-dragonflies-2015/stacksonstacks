f7ec0899929c3061ee0880854c84754dd9c40cf4
Marc Cardacci <marc.cardacci@gmail.com>
2015-07-11 12:35:55 -0400

    adds a create action in comments controller

--- stacksonstacks/app/controllers/comments_controller.rb
+++ stacksonstacks/app/controllers/comments_controller.rb
@@ -4,8 +4,21 @@ class CommentsController < ApplicationController
     @comments = Comment.all
   end
 
-  def new
-    @comment = Comment.new
+  def create
+    @comment = Comment.new(comment_params)
+    @question = Question.find_by(id: comment_params[:commentable_id])
+    if @question.comments << @comment
+      redirect_to :back
+    else
+      flash[:notice] = "There was a problem submitting your comment."
+      redirect_to :back
+    end
   end
 
+
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body, :commentable_id, :commentable_type, :user_id)
+    end
 end
