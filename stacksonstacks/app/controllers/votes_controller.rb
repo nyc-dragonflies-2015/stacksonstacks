6a3ac566053e76b4ab749c2651e22232bec2232b
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-07-11 14:08:00 -0400

    working on create vote

--- stacksonstacks/app/controllers/votes_controller.rb
+++ stacksonstacks/app/controllers/votes_controller.rb
@@ -1,6 +1,17 @@
 class VotesController < ApplicationController
 
   def create
+    p params
+    user = User.find_by(id: session[:user_id])
+    if user
+      if params[:answer_id]
+        Answer.find_by(id: params[:answer_id]).votes.create(user_id: user.id, upvote?: params[:upvote?])
+      else
+        Question.find_by(id: params[:question_id]).votes.create(user_id: user.id, upvote?: params[:upvote?])
+      end
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
