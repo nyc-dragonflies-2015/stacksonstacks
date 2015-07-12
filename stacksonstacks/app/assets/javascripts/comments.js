959c9e9a6660c02ad242eebe86b18215e191fc1c
julia-castro <julia.castro@outlook.com>
2015-07-12 01:23:56 -0400

    edit ajax call; still not working

--- stacksonstacks/app/assets/javascripts/comments.js
+++ stacksonstacks/app/assets/javascripts/comments.js
@@ -2,8 +2,14 @@ $('document').ready(function(){
   $('.comment_for_answer').on("submit", function(event) {
     event.preventDefault();
     $.ajax({
-      url:/comments/new
-      data:
+      // url:/comments/new
+      // data:
+    })
+    .done(function(){
+      console.log("success");
+    })
+    .fail(function(){
+      console.log("error");
     })
   })
 })
