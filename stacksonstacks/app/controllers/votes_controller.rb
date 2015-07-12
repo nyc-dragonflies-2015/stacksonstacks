d02005c9566bc71c539d476c57d747c1bc54d515
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-07-12 15:34:02 -0400

    update votes controller

--- stacksonstacks/app/controllers/votes_controller.rb
+++ stacksonstacks/app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 class VotesController < ApplicationController
 
-  def questionvote
+  def questionvotes
     question = Question.find_by(id: params[:question_id])
     if user_has_made_this_vote(question, params[:question_id])
       flash[:notice] = "You have already made that vote"
@@ -15,7 +15,7 @@ class VotesController < ApplicationController
     end
   end
 
-  def answervote
+  def answervotes
     answer = Answer.find_by(id: params[:answer_id])
     if user_has_made_this_vote(answer, params[:upvote?])
       flash[:notice] = "You have already made that vote"
@@ -30,42 +30,6 @@ class VotesController < ApplicationController
       redirect_to :back
     end
   end
-
-
-
-
-
-  def create
-    if params[:answer_id]
-      answer = Answer.find_by(id: params[:answer_id])
-      if user_has_made_this_vote(answer, params[:upvote?])
-        flash[:notice] = "You have already made that vote"
-        redirect_to :back
-      elsif user_previosly_voted_differently(answer, params[:upvote?])
-        current_user.votes.where(votable_id: answer.id, votable_type: 'Answer').first.delete
-        current_user.votes.where(votable_id: answer.id, votable_type: "Answer")
-        answer.votes.create(user_id: current_user.id, upvote?: params[:upvote?])
-        redirect_to :back
-      else
-        answer.votes.create(user_id: current_user.id, upvote?: params[:upvote?])
-        redirect_to :back
-      end
-    elsif params[:question_id]
-      question = Question.find_by(id: params[:question_id])
-      if user_has_made_this_vote(question, params[:question_id])
-        flash[:notice] = "You have already made that vote"
-        return redirect_to :back
-      elsif user_previosly_voted_differently(question, params[:upvote?])
-        current_user.votes.where(votable_id: question.id, votable_type: 'Question').first.delete
-        question.votes.create(user_id: current_user.id, upvote?: params[:upvote?])
-        redirect_to :back
-      else
-        question.votes.create(user_id: current_user.id, upvote?: params[:upvote?])
-        redirect_to :back
-      end
-    end
-  end
-
 
   private
 
