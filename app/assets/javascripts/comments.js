1165519a4bb46a5e7e27b6f0008066e7a771a824
julia-castro <julia.castro@outlook.com>
2015-07-13 08:47:01 -0400

    comments ajax - still working on

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,18 +1,18 @@
-// $('document').ready(function(){
-//   $('.comment_for_answer').on("submit", function(event) {
-//     event.preventDefault();
-//     // debugger
-//     $target = $(event.target)
-//     $.ajax({
-//       url: '/comments/new',
-//       data: $target.serialize(),
-//       dataType: 'html'
-//     })
-//     .done(function(){
-//       console.log("success");
-//     })
-//     .fail(function(){
-//       console.log("error");
-//     })
-//   })
-// })
+$('document').ready(function(){
+  $('.comment_for_answer').on("submit", function(event) {
+    event.preventDefault();
+    // debugger
+    $target = $(event.target)
+    $.ajax({
+      url: '/comments/new',
+      data: $target.serialize(),
+      dataType: 'html'
+    })
+    .done(function(){
+      console.log("success");
+    })
+    .fail(function(){
+      console.log("error");
+    })
+  })
+})
