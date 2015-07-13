e5067e697dffc844d01a2c86ff0b6f0aca55ed74
julia-castro <julia.castro@outlook.com>
2015-07-13 08:03:53 -0400

    ajax form partly working

--- stacksonstacks/app/assets/javascripts/comments.js
+++ stacksonstacks/app/assets/javascripts/comments.js
@@ -1,9 +1,12 @@
 $('document').ready(function(){
   $('.comment_for_answer').on("submit", function(event) {
     event.preventDefault();
+    // debugger
+    $target = $(event.target)
     $.ajax({
-      // url:/comments/new
-      // data:
+      url: '/comments/new',
+      data: $target.serialize(),
+      dataType: 'html'
     })
     .done(function(){
       console.log("success");
